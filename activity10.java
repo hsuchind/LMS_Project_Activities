0f55e22d721653cb6d3060306eca1ce3348f9804
hsuchind <70679928+hsuchind@users.noreply.github.com>
2020-09-08 19:56:20 -0700

    Update activity10.java

--- activity10.java
+++ activity10.java
@@ -2,15 +2,18 @@ package projectactivity;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class activity10 {
 	
 WebDriver driver;
+WebDriverWait wait;
 	
 	@BeforeMethod
 	
@@ -26,9 +29,33 @@ public void beforeMethod() {
 		driver.findElement(By.cssSelector("#menu-item-1508 > a:nth-child(1)")).click();
 		driver.findElement(By.cssSelector("#post-69 > a:nth-child(2) > img:nth-child(1)")).click();
 		
-		WebElement titlemessage =  driver.findElement(By.cssSelector(".entry-title"));
-		System.out.println("Title Message: "+titlemessage.getText());
-		Assert.assertEquals(titlemessage.getText(), "Social Media Marketing");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("user_login"))); //.visibilityOf(driver.findElement(By.xpath("/html/body/div[1]/div/div/div/main/article/div/section[2]/div[2]/div[2]/div[2]/div[2]/a"))));
+  	  	driver.findElement(By.id("user_login")).sendKeys("root");
+  	  	driver.findElement(By.id("user_pass")).sendKeys("pa$$w0rd");
+  	  	
+  	  	driver.findElement(By.id("wp-submit")).click();
+  	  	
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("menu-item-1508")));
+		  driver.findElement(By.id("menu-item-1508")).click();
+        
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[1]/div/div/div/main/article/div/section[2]/div[2]/div/div/div/div[1]/article/div[2]/p[2]/a")));  
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div/main/article/div/section[2]/div[2]/div/div/div/div[1]/article/div[2]/p[2]/a")).click();;
+        
+        
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[1]/div/div/div/main/article/div/div/div/div/div[3]/div[2]/div[1]/div[1]/a/div[2]")));  
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div/main/article/div/div/div/div/div[3]/div[2]/div[1]/div[1]/a/div[2]")).click();;
+        
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[4]/div[2]/form/input[4]")));  
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[4]/div[2]/form/input[4]")).click();;
+		
+        
+		String Page_title = driver.getTitle();
+		Assert.assertEquals(Page_title, "Monitoring & Digital Advertising – Alchemy LMS");
 }
+	@AfterMethod
+	public void CloseBrowser() {
+		driver.close();
+	}
 
 }
